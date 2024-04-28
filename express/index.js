9ff5683ce5b5137d5a2cc071982c04b73aeccce6
huili.yang <huili.yang@qq.com>
2024-04-28 18:46:57 +0800

    docs: express模块

--- express/index.js
+++ express/index.js
@@ -7,16 +7,27 @@ const port = 3000;
 
 // 所谓中间件，是指业务流程的中间处理环节。
 
-app.use((req, res) => {
-  console.log('req.headers', req.headers);
-  const agent = req.headers['user-agent'];
-  console.log('agent', agent);
-  if(agent.includes('iPhone') || agent.includes('Android')){
-    res.send('iphone or android');
-  }else{
-    res.send('pc');
-  }
-});
+// app.use((req, res,next) => {
+//   const agent = req.headers['user-agent'];
+
+//   console.log('agent', agent);
+//   if(agent.includes('iPhone') || agent.includes('Android')){
+//     res.send('iphone or android');
+//   }else{
+//     res.send('pc');
+//   }
+//   next();
+// });
+
+app.use((req, res,next) => {
+  req.number = 20;
+  res.number = 35;
+  next();
+})
+
+app.use((req, res, next) => {
+  res.send(`<h1>${req.number}+${res.number}</h1>`);
+})
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
